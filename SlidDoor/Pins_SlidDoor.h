7a61d242279492d1d0879076dbaefc2c98f3cdb7
Frank Bruhn <frank-bruhn@outlook.de>
2017-02-02 21:01:08 +0100

    Initial CheckIn

---
+++ SlidDoor/Pins_SlidDoor.h
@@ -0,0 +1,20 @@
+/* 
+	Pin Mapping for SlidDoor 
+
+	modified 02 Feb 2017
+	by Frank Bruhn
+*/
+
+#ifndef Pins_SlidDoor_h
+#define Pins_SlidDoor_h
+
+#define MOTOR_PWM
+#define MOTOR_DIR
+#define MOTOR_CURR
+#define RADAR_INDOOR
+#define RADAR_OUTDOOR
+#define SWITCH_LOCK
+
+#endif
+
+
